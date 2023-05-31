39c615583618921150524acda85b732cc7503d4f
cristian007369 <cristiankira123@gmail.com>
2023-05-31 17:13:33 -0500

    Enfermedades mucosas

--- diccionario_coloquial.py
+++ diccionario_coloquial.py
@@ -74,6 +74,7 @@ def ver_base_datos_palabras():
     t_resultados.insert(INSERT, f"Palabras:\n")
     for fila in resultados:
         t_resultados.insert(INSERT, f"*{fila[0]}\n")
+    t_resultados.config(state="disabled")
 def ver_base_datos_expresiones():
     toplevel_palabras = Toplevel()
     toplevel_palabras.title("Expresiones disponibles")
@@ -91,6 +92,7 @@ def ver_base_datos_expresiones():
     t_resultados.insert(INSERT, f"Expresiones:\n")
     for fila in resultados:
         t_resultados.insert(INSERT, f"*{fila[0]}\n")
+    t_resultados.config(state="disabled")
 def agregar_nueva(): 
     global toplevel_agregar1
     bt_agregar.config(state="disabled")   
@@ -125,7 +127,6 @@ def precionar_no():
     t_resultados_es.config(state="disabled")
 def agregar_nueva_palabra():
     global toplevel_agregar2
-    region=["Caribe", "Pacífico","Orinoquía", "Amazonía", "Andina","Insular"]
     toplevel_agregar1.destroy()
     toplevel_agregar2 = Toplevel()
     toplevel_agregar2.title("Nueva palabra")
@@ -172,7 +173,52 @@ def agregar_nueva_palabra():
     bt_guardar.config(state="active")
     bt_guardar.place(x=86, y=348, width=100, height=50)
 def agregar_nueva_expresion():
-    pass
+    global toplevel_agregar3
+    toplevel_agregar1.destroy()
+    toplevel_agregar3 = Toplevel()
+    toplevel_agregar3.title("Nueva Expresión")
+    toplevel_agregar3.resizable(False, False)
+    toplevel_agregar3.config(bg="white")
+    toplevel_agregar3.geometry("272x418")
+
+    label_imagen = Label(toplevel_agregar3,bg="white",image=barra_texto)
+    label_imagen.place(x=10, y=20)
+
+    text_uno = Text(toplevel_agregar3, bg="white", fg="gray", font=("Times New Roman", 25), bd=0, width=13, height=1)
+    text_uno.insert("1.0", f"{busqueda1}")
+    text_uno.config(state="disabled")
+    text_uno.place(x=25, y=26)
+
+    label_imagen = Label(toplevel_agregar3,bg="white",image=barra_texto)
+    label_imagen.place(x=10, y=102)
+
+    entry_definicion = Entry(toplevel_agregar3, textvariable=definicion)
+    entry_definicion.config(bg="white", fg="gray", font=("Times New Roman", 19), width=19,bd=0)
+    entry_definicion.insert(0, "Definición")
+    entry_definicion.bind("<FocusIn>", lambda event: entry_definicion.delete(0, "end") if definicion.get() == "Definición" else None)
+    entry_definicion.bind("<Key>",lambda event: entry_definicion.config(fg="Black"))
+    entry_definicion.bind("<Return>", lambda event: entry_traducion.focus_set())
+    entry_definicion.place(x=25,y=112)
+
+    label_imagen = Label(toplevel_agregar3,bg="white",image=barra_texto)
+    label_imagen.place(x=10, y=184)
+
+    entry_traducion = Entry(toplevel_agregar3, textvariable=traducion)
+    entry_traducion.config(bg="white", fg="gray", font=("Times New Roman", 19), width=19,bd=0)
+    entry_traducion.insert(0, "Traducción")
+    entry_traducion.bind("<FocusIn>",lambda event: entry_traducion.delete(0, "end") if traducion.get() == "Traducción" else None)
+    entry_traducion.bind("<Key>",lambda event: entry_traducion.config(fg="Black"))
+    entry_traducion.bind("<Return>", lambda event: cmb_region.focus_set())
+    entry_traducion.place(x=25,y=194)
+
+    cmb_region = ttk.Combobox(toplevel_agregar3, textvariable=region_selected, values=region, font=("times new roman", 20))
+    cmb_region.set("Región")
+    cmb_region.bind("<FocusIn>", lambda event: cmb_region.event_generate("<Down>") if region_selected.get()=="Región" else None)
+    cmb_region.place(x=15, y=270,width=247,height=50)
+ 
+    bt_guardar = Button(toplevel_agregar3,bg="white",text="Guardar", command=guardar_nueva_expresion)
+    bt_guardar.config(state="active")
+    bt_guardar.place(x=86, y=348, width=100, height=50)
 def guardar_nueva_palabra():
     if traducion.get()!="" and traducion.get()!="Traducción" and definicion.get()!="" and definicion.get()!="Definición" and region_selected.get()!="Región":
         conn.execute('CREATE TABLE IF NOT EXISTS diccionario (palabra TEXT, definicion TEXT, traduccion TEXT, region TEXT)')
@@ -181,20 +227,28 @@ def guardar_nueva_palabra():
         toplevel_agregar2.destroy()
     else:
         messagebox.showinfo(".____.", "No seas estúpido")
-
+def guardar_nueva_expresion():
+    if traducion.get()!="" and traducion.get()!="Traducción" and definicion.get()!="" and definicion.get()!="Definición" and region_selected.get()!="Región":
+        conn1.execute('CREATE TABLE IF NOT EXISTS diccionario (expresion TEXT, definicion TEXT, traduccion TEXT, region TEXT)')
+        conn1.execute("INSERT INTO diccionario (expresion, definicion, traduccion, region) VALUES (?, ?, ?, ?)", (busqueda1, definicion.get(), traducion.get(), region_selected.get()))
+        conn1.commit()
+        toplevel_agregar3.destroy()
+    else:
+        messagebox.showinfo(".____.", "No seas estúpido")
 
 ventana_principal = Tk()
 ventana_principal.title("Diccionario coloquial")
-ventana_principal.geometry("800x500")
+ventana_principal.geometry("760x500")
 ventana_principal.resizable(False, False)
 ventana_principal.config(bg="white")
 
-global t_resultados_es,t_resultados_in,busqueda, conn, conn1, bt_agregar, barra_texto, definicion, traducion, region_selected
+global t_resultados_es,t_resultados_in,busqueda, conn, conn1, bt_agregar, barra_texto, definicion, traducion, region_selected, region
 
 conn = sqlite3.connect('mi_diccionario_palabras.db')
 conn1 = sqlite3.connect('mi_diccionario_expresiones.db')
 barra_texto=PhotoImage(file="barra_texto1.png")
 barra=PhotoImage(file="barra_busqueda.png")
+region=["Caribe", "Pacífico","Orinoquía", "Amazonía", "Andina","Insular"]
 region_selected= StringVar()
 busqueda = StringVar()
 definicion = StringVar()
@@ -214,21 +268,21 @@ entry_busqueda.place(x=77,y=20)
 # areas de texto para los resultados
 t_resultados_es = Text(ventana_principal)
 t_resultados_es.config(bg="white", fg="black", font=("times new roman", 20),state="disabled")
-t_resultados_es.place(x=30,y=80,width=370,height=340)
+t_resultados_es.place(x=30,y=80,width=350,height=340)
 
 t_resultados_in = Text(ventana_principal)
 t_resultados_in.config(bg="white", fg="black", font=("times new roman", 20),state="disabled")
-t_resultados_in.place(x=400,y=80,width=370,height=340)
+t_resultados_in.place(x=380,y=80,width=350,height=340)
 
 # botones 
 bt_palabras = Button(ventana_principal,bg="white",text="Palabras", command=ver_base_datos_palabras)
 bt_palabras.place(x=30, y=440, width=150, height=50)
 
 bt_expresiones = Button(ventana_principal,bg="white",text="Expresiones", command=ver_base_datos_expresiones)
-bt_expresiones.place(x=325, y=440, width=150, height=50)
+bt_expresiones.place(x=305, y=440, width=150, height=50)
 
 bt_agregar = Button(ventana_principal,bg="white",text="Agregar nueva", command=agregar_nueva)
 bt_agregar.config(state="disabled")
-bt_agregar.place(x=620, y=440, width=150, height=50)
+bt_agregar.place(x=580, y=440, width=150, height=50)
 
 ventana_principal.mainloop()
